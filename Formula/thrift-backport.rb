ec058a4a7720687ca78b0a0fd5c3acb9d9480458
Hans Duedal <hd@onlinecity.dk>
2014-01-08 09:48:10 +0100

    With the extension separate revert to tarball and other fixes

--- Formula/thrift-backport.rb
+++ Formula/thrift-backport.rb
@@ -2,19 +2,16 @@ require 'formula'
 
 class ThriftBackport < Formula
   homepage 'http://thrift.apache.org'
-  # The thrift.apache.org 0.9.1 archive is missing PHP ext, fixed in THRIFT-2129
-  # By grapping the source from git instead, it's fixed, but we need to bootstrap
-  url 'https://git-wip-us.apache.org/repos/asf/thrift.git', :branch => "0.9.1"
-  version "0.9.1"
+  url 'http://archive.apache.org/dist/thrift/0.9.1/thrift-0.9.1.tar.gz'
+  sha1 'dc54a54f8dc706ffddcd3e8c6cd5301c931af1cc'
 
   head do
     url 'https://git-wip-us.apache.org/repos/asf/thrift.git', :branch => "master"
-    #depends_on :autoconf
-    #depends_on :automake
-    #depends_on :libtool
+    depends_on :autoconf
+    depends_on :automake
+    depends_on :libtool
+    depends_on 'pkg-config' => :build
   end
-
-  env :userpaths  # To find brew'ed php (, upstream uses `php-config` some places)
 
   # We need to install from sources, because website tarball is missing php-ext
   depends_on :autoconf
@@ -29,9 +26,6 @@ class ThriftBackport < Formula
 
   depends_on 'boost'
   depends_on :python => :optional
-  depends_on "php53" => [:optional, 'with-php'] if Formula.factory("php53").linked_keg.exist?
-  depends_on "php54" => [:optional, 'with-php'] if Formula.factory("php54").linked_keg.exist?
-  depends_on "php55" => [:optional, 'with-php'] if Formula.factory("php55").linked_keg.exist?
 
   # Patches required to compile 0.9.1 with "-std=c++11", maybe remove when thrift 1.0 hits
   def patches
@@ -51,23 +45,10 @@ class ThriftBackport < Formula
     ]
   end
 
-  def php_conf_d
-    if Formula.factory("php53").linked_keg.exist?
-      "#{HOMEBREW_PREFIX}/etc/php/5.3/conf.d"
-    elsif Formula.factory("php54").linked_keg.exist?
-      "#{HOMEBREW_PREFIX}/etc/php/5.4/conf.d"
-    elsif Formula.factory("php55").linked_keg.exist?
-      "#{HOMEBREW_PREFIX}/etc/php/5.5/conf.d"
-    else
-      ""
-    end
-  end
-
   def install
-    # system "./bootstrap.sh" if build.head?
-    system "./bootstrap.sh" # always install from source
+    system "./bootstrap.sh" if build.head?
 
-    exclusions = ["--without-ruby"]
+    exclusions = ["--without-ruby", "--without-tests", "--without-php_extension"]
 
     exclusions << "--without-python" unless build.with? "python"
     exclusions << "--without-haskell" unless build.include? "with-haskell"
@@ -76,13 +57,10 @@ class ThriftBackport < Formula
     exclusions << "--without-php" unless build.include? "with-php"
     exclusions << "--without-erlang" unless build.include? "with-erlang"
 
-    ENV["PY_PREFIX"] = prefix  # So python bindins don't install to /usr!
-    ENV["CXXFLAGS"] = "-std=c++11" # Required on OS 10.9, see THRIFT-1458 & THRIFT-2229
+    ENV.cxx11 if MacOS.version >= :mavericks && ENV.compiler == :clang
 
-    if build.include? "with-php"
-      ENV["PHP_PREFIX"] = "#{HOMEBREW_PREFIX}/lib"
-      ENV["PHP_CONFIG_PREFIX"] = "#{php_conf_d}"
-    end
+    ENV["PY_PREFIX"] = prefix  # So python bindins don't install to /usr!
+    ENV["PHP_PREFIX"] = prefix # So PHP bindings don't install to /usr!
 
     system "./configure", "--disable-debug",
                           "--prefix=#{prefix}",
@@ -99,8 +77,8 @@ class ThriftBackport < Formula
       gem install thrift
 
     To install PHP bindings:
-      Install brew'ed php
       brew install thrift-backport --with-php
+      extension available from josegonzalez/php tap
 
     EOS
   end
